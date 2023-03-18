52bc45376eee65ce0ea35eb6f07456dd7b2ef2d1
Enrique Irazoqui Ruelas <84408487+EnriqueIrazoqui@users.noreply.github.com>
2023-03-18 16:45:25 -0600

    Add files via upload

---
+++ controllers/automovil_controller.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const pool = require('../config/mariadb.js');
+
+const post_automovil = (req, res) => {
+
+}
+
+const get_automovil = (req, res) => {
+
+}
+
+const put_automovil = (req, res) => {
+
+}
+
+const delete_automovil = (req, res) => {
+
+}
+
+module.exports = {
+    post_automovil,
+    get_automovil,
+    put_automovil,
+    delete_automovil
+}
