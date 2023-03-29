8962b265aed6d34ac676898fc0b1d70032a2b28c
NicolasB7 <za19012207@zapopan.tecmm.edu.mx>
2023-03-29 16:43:34 -0600

    GET, POST, PUT, DELETE Automovil

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-
-const swaggerUi = require('swagger-ui-express'),
-swaggerDocument = require('./swagger.json');
+require("dotenv").config();
 
 app.listen(process.env.NODE_PORT, () => {
-    console.log('server running '+process.env.NODE_PORT);
+    console.log("server running " + process.env.NODE_PORT);
 });
-
-app.use(
-    '/api-docs',
-    swaggerUi.serve, 
-    swaggerUi.setup(swaggerDocument)
-);
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
     next();
 });
 
 app.use('/automovil', require('./routes/automovil_routes'));
-app.use('/conductor', require('./routes/conductor_routes'));
--- routes/automovil_routes.js
+++ routes/automovil_routes.js
@@ -5,12 +5,33 @@ const { body } = require("express-validator");
 
 const { post_automovil, get_automovil, put_automovil, delete_automovil } = require('../controllers/automovil_controller.js');
 
-router.post('/', post_automovil);
+router.post('/',
+    body('idCampus').exists().notEmpty().isString(),
+    body('marca').exists().notEmpty().isString(),
+    body('modelo').exists().notEmpty().isString(),
+    body('placas').exists().notEmpty().isString(),
+    body('color').exists().notEmpty().isString(),
+    body('generacion').exists().notEmpty().isString(),
+    body('serie').exists().notEmpty().isString(),
+    body('tipoCombustible').exists().notEmpty().isString(),
+    post_automovil);
 
 router.get('/', get_automovil);
 
-router.put('/', put_automovil);
+router.put('/',
+    body('idVehiculo').exists().notEmpty().isNumeric().not().isString(),
+    body('idCampus').exists().notEmpty().isString(),
+    body('marca').exists().notEmpty().isString(),
+    body('modelo').exists().notEmpty().isString(),
+    body('placas').exists().notEmpty().isString(),
+    body('color').exists().notEmpty().isString(),
+    body('generacion').exists().notEmpty().isString(),
+    body('serie').exists().notEmpty().isString(),
+    body('tipoCombustible').exists().notEmpty().isString(),
+    put_automovil);
 
-router.delete('/', delete_automovil);
+router.delete('/',
+    body('idVehiculo').exists().notEmpty().isNumeric().not().isString(),
+    delete_automovil);
 
 module.exports = router
